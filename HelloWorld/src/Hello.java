55503fde124e97052d3478f83b3e2c9d7620cae7
Kelvin <kelvin.mozart@live.com>
2019-05-19 19:29:21 -0300

    helloWorld

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Kelvin!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdnumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdnumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+    }
+}
